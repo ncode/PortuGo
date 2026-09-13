33f4321e0d4fe9ab481bb66ace3e60f8ee6bcc96
Juliano Martinez <juliano@martinez.io>
2026-09-13 21:38:05 +0200

    test: add declaration aggregate golden

---
+++ internal/parser/declaration_golden_test.go
@@ -0,0 +1,55 @@
+package parser
+
+import (
+	"bytes"
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/ncode/portugol-go/internal/ast"
+	"github.com/ncode/portugol-go/internal/lexer"
+	"github.com/ncode/portugol-go/internal/source"
+)
+
+func TestAggregateDeclarationGolden(t *testing.T) {
+	sourcePath := "../../testdata/format/aggregate_declarations.alg"
+	wantPath := "../../testdata/format/aggregate_declarations.formatted.alg"
+	original, err := source.ReadFile(sourcePath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	want, err := os.ReadFile(wantPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, tokens, lexDiags := lexer.Scan(sourcePath, original)
+	if len(lexDiags) != 0 {
+		t.Fatalf("lexer diagnostics: %v", lexDiags)
+	}
+	program, parseDiags := Parse(tokens)
+	if len(parseDiags) != 0 {
+		t.Fatalf("parser diagnostics: %v", parseDiags)
+	}
+	var formatted bytes.Buffer
+	if err := ast.Fprint(&formatted, program); err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(formatted.Bytes(), want) {
+		t.Fatalf("formatted declaration golden mismatch:\ngot:\n%s\nwant:\n%s", formatted.Bytes(), want)
+	}
+	_, reparsedTokens, lexDiags := lexer.Scan("aggregate_declarations.alg", formatted.String())
+	if len(lexDiags) != 0 {
+		t.Fatalf("reformatted lexer diagnostics: %v", lexDiags)
+	}
+	reparsed, parseDiags := Parse(reparsedTokens)
+	if len(parseDiags) != 0 {
+		t.Fatalf("reformatted parser diagnostics: %v", parseDiags)
+	}
+	program.Fragments = nil
+	reparsed.Fragments = nil
+	clearSyntaxPositions(reflect.ValueOf(program))
+	clearSyntaxPositions(reflect.ValueOf(reparsed))
+	if !reflect.DeepEqual(program, reparsed) {
+		t.Fatal("aggregate declaration golden changed the syntax tree")
+	}
+}
